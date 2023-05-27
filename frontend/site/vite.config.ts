9d8dbdb633763416e1c3391cc6c2f0591129b8c6
Mehmet Kırkoca <mehmetk.1987@gmail.com>
2023-05-27 09:32:16 +0300

    site created

---
+++ frontend/site/vite.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server:{
+    host:"site",
+    port:3001,
+    strictPort:true
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
